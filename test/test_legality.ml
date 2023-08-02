9ab61fbc5f57d3be0a1ea85fa10cdba1634fbd76
ambrona <mac.ambrona@gmail.com>
2023-08-02 12:14:12 +0200

    Test: wrap legality tests into submodule

--- test/test_legality.ml
+++ test/test_legality.ml
@@ -26,52 +26,53 @@ module Internal = struct
   open Legality
   open Board.Square
 
-  let empty_state fen =
-    Rules.{ pos = Position.of_fen fen; events = EventSet.empty }
+  module TestRules = struct
+    let empty_state fen =
+      Rules.{ pos = Position.of_fen fen; events = EventSet.empty }
 
-  let test_static_rule () =
-    List.iter
-      (fun (fen, expected_static, expected_non_static) ->
-        let state = Rules.apply (empty_state fen) Rules.[ static_rule ] in
-        let static s = EventSet.mem (Event.Static s) state.events in
-        assert (List.for_all static expected_static);
-        assert (List.for_all (Fun.negate static) expected_non_static))
-      [
-        ( "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-          [ a1; c1; e1; f1; h1; d2; a8; c8; d8; e8; f8; h8; e7 ],
-          [ b1; g1; b8; g8 ] );
-        ( "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 1",
-          [ d1; e1; d8; e8 ],
-          [] );
-        ( "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1",
-          [ a1; c1; e1; h1; d2; a8; c8; d8; e8; f8; h8 ],
-          [ d1; f1; e2 ] );
-        ("4k3/8/8/8/8/8/1P1P4/2B1K3 w K - 0 1", [ c1; b2; d2 ], [ e1 ]);
-      ]
+    let test_static_rule () =
+      List.iter
+        (fun (fen, expected_static, expected_non_static) ->
+          let state = Rules.apply (empty_state fen) Rules.[ static_rule ] in
+          let static s = EventSet.mem (Event.Static s) state.events in
+          assert (List.for_all static expected_static);
+          assert (List.for_all (Fun.negate static) expected_non_static))
+        [
+          ( "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+            [ a1; c1; e1; f1; h1; d2; a8; c8; d8; e8; f8; h8; e7 ],
+            [ b1; g1; b8; g8 ] );
+          ( "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 1",
+            [ d1; e1; d8; e8 ],
+            [] );
+          ( "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1",
+            [ a1; c1; e1; h1; d2; a8; c8; d8; e8; f8; h8 ],
+            [ d1; f1; e2 ] );
+          ("4k3/8/8/8/8/8/1P1P4/2B1K3 w K - 0 1", [ c1; b2; d2 ], [ e1 ]);
+        ]
 
-  let test_material_rule () =
-    List.iter
-      (fun (fen, invalid) ->
-        let state = Rules.apply (empty_state fen) Rules.[ material_rule ] in
-        Helpers.print_events state.events;
-        assert (invalid = EventSet.mem Event.Contradiction state.events))
-      [
-        ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", false);
-        ("rnbqkbnr/pppppppp/8/8/8/P7/PPPPPPPP/1NBQKBNR w KQkq - 0 1", true);
-        ("rnbqkbnr/pppppppp/8/8/8/B7/PPPPPPPP/RNBQKBNR w KQkq - 0 1", true);
-        ("rnbqkbnr/pppppppp/8/8/8/B7/PPPPPPPP/RN1QKBNR w KQkq - 0 1", true);
-        ("rqrqkb1r/p1b4p/p6p/p6p/8/8/8/4K3 w - - ? 1", true);
-        ("rqr1kb1r/p1b4p/p6p/p6p/8/8/8/4K3 w - - ? 1", false);
-      ]
+    let test_material_rule () =
+      List.iter
+        (fun (fen, invalid) ->
+          let state = Rules.apply (empty_state fen) Rules.[ material_rule ] in
+          assert (invalid = EventSet.mem Event.Contradiction state.events))
+        [
+          ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", false);
+          ("rnbqkbnr/pppppppp/8/8/8/P7/PPPPPPPP/1NBQKBNR w KQkq - 0 1", true);
+          ("rnbqkbnr/pppppppp/8/8/8/B7/PPPPPPPP/RNBQKBNR w KQkq - 0 1", true);
+          ("rnbqkbnr/pppppppp/8/8/8/B7/PPPPPPPP/RN1QKBNR w KQkq - 0 1", true);
+          ("rqrqkb1r/p1b4p/p6p/p6p/8/8/8/4K3 w - - ? 1", true);
+          ("rqr1kb1r/p1b4p/p6p/p6p/8/8/8/4K3 w - - ? 1", false);
+        ]
 
-  let tests =
-    Alcotest.
-      [
-        test_case "static_rule" `Quick test_static_rule;
-        test_case "material_rule" `Quick test_material_rule;
-      ]
+    let tests =
+      Alcotest.
+        [
+          test_case "static_rule" `Quick test_static_rule;
+          test_case "material_rule" `Quick test_material_rule;
+        ]
+  end
 end
 
 let () =
   let open Alcotest in
-  run "Legality" [ ("Legality.Internal", Internal.tests) ]
+  run "Legality.Internal" [ ("Rules", Internal.TestRules.tests) ]
